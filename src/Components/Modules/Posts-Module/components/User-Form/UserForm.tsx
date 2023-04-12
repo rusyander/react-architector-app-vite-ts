7521c9c5d9bd0d1480e06a8a077a1566a2d39ea3
rusyander <rustam.urmanov.1996@mail.ru>
2023-04-12 20:59:56 +0500

    complite userForm

--- src/Components/Modules/Posts-Module/components/User-Form/UserForm.tsx
+++ src/Components/Modules/Posts-Module/components/User-Form/UserForm.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import './userForm.css';
 
+import UserImage from '../../../../../assets/images/userIcon.svg';
+import Avatar from '../../../../UI/Avatar/Avatar';
+import InputPostFilds from '../../../../Components/Input-Post-Fields/InputPostFilds';
+
 export default function UserForm() {
-  return <div>UserForm</div>;
+  const [formValue, setFormValue] = React.useState<any>([]);
+  return (
+    <div className="UserFormsMain">
+      <div className="UserFormsMainImages">
+        <Avatar imageUrl={UserImage} avatar="default" />
+      </div>
+
+      <InputPostFilds
+        placeholder="Start a post"
+        onChange={(data) => setFormValue(data)}
+        size="small"
+      />
+    </div>
+  );
 }
